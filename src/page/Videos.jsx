abb45f35f420853b17c253dfef406ac05f17f977
HoWoon Jung <jhu940810@gmail.com>
2023-10-15 18:14:12 +0900

    Feat: search, popular video card implement(mock data)

--- src/page/Videos.jsx
+++ src/page/Videos.jsx
@@ -1,7 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router';
+import VideoCard from '../components/VideoCard';
+import { search } from '../api/youtube';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>{keyword ? `${keyword}` : 'popular'}</div>;
+
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(['videos', keyword], () => search(keyword));
+
+  return (
+    <>
+      {isLoading && <p>로딩중...</p>}
+      {error && <p>에러 발생!</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
